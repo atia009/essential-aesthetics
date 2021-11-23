b9a364555b8da7daf5315bd0db86b337a5137a51
atia009 <aarontia009@gmail.com>
2021-11-23 13:49:32 -0800

    Created services.js to dynamically load html content. Created showcaseObject and servicesList to hold htmlinfo. Created updateActive, loadHtml, loadShowcase, and loadServices functions.

---
+++ js/services.js
@@ -0,0 +1,101 @@
+// objects
+showcaseObject =
+{
+ icon: "fas fa-angle-down",
+ src: "../images/services-showcase.jpg",
+ title: "Services",
+};
+
+servicesList =
+[
+ {
+  title: "Laser Hair Removal",
+  body: "Lorem ips  icing elit. Quo sequi non totam eveniet recusandae dignissimos sint provident impedit veniam similique.",
+  btnLink: "#",
+  btnText: "Learn More",
+ },
+ {
+  title: "Botox",
+  body: "Lorem ips  icing elit. Quo sequi non totam eveniet recusandae dignissimos sint provident impedit veniam similique.",
+  btnLink: "#",
+  btnText: "Learn More",
+ },
+ {
+  title: "Acne Scar Removal",
+  body: "Lorem ips  icing elit. Quo sequi non totam eveniet recusandae dignissimos sint provident impedit veniam similique.",
+  btnLink: "#",
+  btnText: "Learn More",
+ },
+ {
+  title: "Facial Volume Loss",
+  body: "Lorem ips  icing elit. Quo sequi non totam eveniet recusandae dignissimos sint provident impedit veniam similique.",
+  btnLink: "#",
+  btnText: "Learn More",
+ },
+ {
+  title: "Hand Rejuvenation",
+  body: "Lorem ips  icing elit. Quo sequi non totam eveniet recusandae dignissimos sint provident impedit veniam similique.",
+  btnLink: "#",
+  btnText: "Learn More",
+ },
+]
+
+// variables
+const showcase = document.querySelector(".showcase");
+const services = document.querySelector(".services");
+
+// functions
+function updateActive()
+{
+ const pages = document.querySelectorAll(".page__link");
+ pages.forEach(function(page) 
+ {
+  if (page.innerHTML === "Services")
+  {
+   page.classList.add("active");
+  }
+  else 
+  {
+   page.classList.remove("active");
+  }
+ })
+}
+
+function loadHtml()
+{
+ loadShowcase();
+ loadServices();
+}
+
+function loadShowcase()
+{
+  showcase.innerHTML = `<h1 class="showcase__title">${showcaseObject.title}</h1>
+                       <button class="showcase__btn">
+                        <i class="showcase__icon ${showcaseObject.icon}"></i>
+                       </button>`
+  showcase.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("${showcaseObject.src}")`;
+}
+
+function loadServices()
+{
+ serviceItems = servicesList.map(function(service)
+ {
+  return `<li class="service">
+         <div class="service__inner">
+           <h3 class="service__title">${service.title}</h3>
+           <p class="service__body">${service.body}</p>
+           <button class="service__btn">
+             <a href="${service.btnLink}" class="service__link">${service.btnText}</a>
+           </button>
+         </div>
+      </li>`
+ })
+  services.insertAdjacentHTML(`beforeend`, serviceItems.join(""));
+}
+
+// event listeners
+window.addEventListener("DOMContentLoaded", function() 
+{
+ updateActive();
+ loadHtml();
+})
